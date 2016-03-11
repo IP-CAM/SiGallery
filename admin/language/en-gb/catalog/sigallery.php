457713d9b1cba18d6740e264be510c235cf7ea34
SvM <svmidi@gmail.com>
2016-03-11 16:52:40 +0300

    fix language

--- admin/language/en-gb/catalog/sigallery.php
+++ admin/language/en-gb/catalog/sigallery.php
@@ -1,18 +1,15 @@
 <?php
 // Heading
 $_['heading_title']            = 'Simple Image Gallery';
-
 // Text
 $_['text_success']             = 'Success: Changes made successfully!';
 $_['text_default']             = 'Default';
 $_['text_image_manager']       = 'Image manager';
 $_['text_list']                = 'list SIGallery';
-
 // Column
 $_['column_name']              = 'Name Gallery';
 $_['column_status']            = 'Status';
 $_['column_action']            = 'Action';
-
 // Entry
 $_['entry_name']               = 'Name Gallery:';
 $_['entry_title']              = 'Title:';
@@ -24,44 +21,42 @@ $_['entry_meta_description']   = 'Meta-description:';
 $_['entry_meta_keyword']       = 'Meta-keyword:';
 $_['entry_meta_title']         = 'Meta-title:';
 $_['entry_status']             = 'Status:';
-$_['entry_store']              = 'Store:';
 $_['entry_sort_order']         = 'Sort order:';
 $_['entry_keyword']            = 'SEO URL:';
-$_['entry_width']              = 'width:';
 $_['entry_type']               = 'Gallery type:';
+$_['entry_width']              = 'width:';
 $_['entry_height']             = 'Height:';
-$_['entry_autoplay']           = 'Autoplay (ms):';
-$_['entry_parent']             = 'Parent:';
-$_['button_add_gallimage']     = 'Add image';
 $_['entry_width_popup']        = 'Images width popup:';
 $_['entry_height_popup']       = 'Images height popup:';
+$_['entry_autoplay']           = 'Autoplay (ms):';
+$_['entry_parent']             = 'Parent:';
 $_['entry_gallery_image']      = 'Gallery image';
+// Buttons
+$_['button_add_sigallery']     = 'Add image';
+$_['button_remove']            = 'Delete image';
+$_['button_cancel']            = 'Cancel';
+$_['button_save']              = 'Save';
+$_['button_edit']              = 'Edit';
 // Tabs
 $_['tab_general']              = 'Description';
 $_['tab_images']               = 'Images';
 $_['tab_data']                 = 'Data';
 $_['tab_settings']             = 'Settings';
-
 // Help
 $_['help_keyword']             = 'Replace spaces with sign -. The value of this field must be unique on the entire system.';
 $_['help_order']               = '-1 to hide';
 $_['help_autoplay']            = '0 - disable autoplay.';
-
+$_['help_popup']               = '0 - Full size.';
 // Text
-$_['text_confirm']             = 'Вы действительно желаете удалить галерею?';
-$_['text_parent']              = '- root -';
-
+$_['text_confirm']             = 'Are you sure you want to delete the gallery?';
 // Error
 $_['error_permission']         = 'Warning: У вас не достаточно прав для внесения изменений в галерею!';
 $_['error_name']               = 'Gallery name must contain between 3 and 64 characters!';
 $_['error_title']              = 'Title name must contain between 3 and 64 characters!';
 $_['error_root']               = 'Root category can not be deleted!';
 $_['error_keyword']            = 'SEO URL must contain between 3 and 64 characters!';
-$_['error_default']            = 'Warning: Этот слой невозможно удалить, так как он является слоем по усолчанию!';
-$_['error_product']            = 'Warning: Этот слой невозможно удалить, так как он связан с %s продуктами!';
-$_['error_category']           = 'Warning: Этот слой невозможно удалить, так как он связан с %s категориями!';
-$_['error_information']        = 'Warning: Этот слой невозможно удалить, так как он связан с %s информационными страницами!';
-
+$_['error_category']           = 'Warning: This layer can not be deleted because it is associated with %s categories!';
+$_['error_parent']             = 'You can not remove the gallery %s, as it is for the parent %s';
 // Types gallery
 $_['type_fade']                = 'Fade';
 $_['type_single']              = 'One image';
